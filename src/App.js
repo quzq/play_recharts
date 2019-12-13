7c845ef401ae6f3c3bfbe76ccd867108bdc9e2b3
quzq <quzyam@gmail.com>
2019-12-13 17:51:50 +0900

    add sample charts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SimpleLineChart from './containers/SimpleLineChart'
+import SimpleBarChart from './containers/SimpleBarChart'
+import ThreeDimScatterChart from './containers/ThreeDimScatterChart'
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SimpleLineChart/>
+      <SimpleBarChart/>
+      <ThreeDimScatterChart/>
     </div>
   );
 }

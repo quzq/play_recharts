ae99876df801e95cb69b4f0d8d04d54aa30e2373
quzq <quzyam@gmail.com>
2019-12-13 18:03:51 +0900

    change param threeDimChart

--- src/containers/ThreeDimScatterChart.js
+++ src/containers/ThreeDimScatterChart.js
@@ -11,7 +11,7 @@ const data01 = [
 const data02 = [
   { x: 200, y: 260, z: 240 }, { x: 240, y: 290, z: 220 },
   { x: 190, y: 290, z: 250 }, { x: 198, y: 250, z: 210 },
-  { x: 180, y: 280, z: 260 }, { x: 210, y: 220, z: 230 },
+  { x: 180, y: 280, z: 260 }, { x: 210, y: 220, z: 530 },
 ];
 
 export default class Example extends PureComponent {
@@ -33,7 +33,7 @@ export default class Example extends PureComponent {
         <Tooltip cursor={{ strokeDasharray: '3 3' }} />
         <Legend />
         <Scatter name="A school" data={data01} fill="#8884d8" shape="circle" />
-        <Scatter name="B school" data={data02} fill="#82ca9d" shape="triangle" />
+        <Scatter name="B school" data={data02} fill="#82ca9d" shape="circle" />
       </ScatterChart>
     );
   }
